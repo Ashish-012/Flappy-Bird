1fea19a9d305463950370abed7d1a9b835c7dd42
unknown <contactashishjoshi99@gmail.com>
2020-10-02 23:57:58 +0530

    added pipes in game

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 pygame.init()
 
 
@@ -9,7 +10,21 @@ def draw_floor(floor_x):
     if floor_x <= -550:
        return 0
     return floor_x
- 
+
+def create_pipe():
+    pipe_rect = pipe_surface.get_rect(midtop = (500,400))
+    return pipe_rect
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 4
+        if pipe.centerx < -20:
+            pipes.remove(pipe)
+    return pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        window.blit(pipe_surface,pipe)
 
 # Screen Resolution
 width = 550
@@ -34,28 +49,44 @@ bird = pygame.image.load('assets/bluebird-midflap.png')
 bird = pygame.transform.scale(bird,(45,35))
 bird_rect = bird.get_rect(center = (100,height//2))
 
+pipe_surface = pygame.image.load('assets/pipe-green.png')
+pipe_surface = pygame.transform.scale(pipe_surface,(90,200))
+
+pipes = []
+pipe_height = []
+spawn_pipe = pygame.USEREVENT
+pygame.time.set_timer(spawn_pipe,1200)
 
 floor_x = 0
 gravity = 0.25
 bird_movement = 0
 
-while True:
+play = True
+
+while play:
     
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            play = False
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
                 bird_movement -= 8
-
+        if event.type == spawn_pipe:
+            pipes.append(create_pipe())
+            print(pipes)
+            
                 
     #drawing the background
 
     window.blit(background,(0,0))  
 
+    #drawing the pipes
+    pipes = move_pipes(pipes)
+    draw_pipes(pipes)
 
     #drawing the floor
     
@@ -68,6 +99,9 @@ while True:
     bird_rect.centery += bird_movement
     window.blit(bird,bird_rect)
 
+    
+
+    #updating game window
     pygame.display.update()
     clock.tick(FPS)
 
